d0747c5d6e6c3606ab9ec3f0276558cb726d788c
Marco Francisco <inbox.marcofrancisco@gmail.com>
2025-06-09 17:34:07 +0100

    this is tarting to look KAWAI DESU NEEEE

--- Poketo/BookTracker.swift
+++ Poketo/BookTracker.swift
@@ -21,85 +21,77 @@ struct BookTracker : View {
     }
     
     var body: some View {
-            NavigationStack {
-                List(books) { book in
-                    HStack {
-                        AsyncImage(url: book.coverURL()) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView()
-                                    .frame(width: 50, height: 50)
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 50, height: 50)
-                                    .clipShape(Circle())
-                                    .clipped()
-                            case .failure:
-                                Image(systemName: "photo")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 50, height: 50)
-                                    .clipShape(Circle())
-                                    .clipped()
-                            @unknown default:
-                                EmptyView()
-                            }
-                        }
-                        
-                        Spacer()
-                        
-                        Text(book.title)
-                        
-                        Spacer()
-                        
-                        VStack(spacing: 10) {
-                            var totalNumberOfPages = Int.random(in: 0...1000)
-                            var totalNumberReadPages = Int.random(in: 0...totalNumberOfPages)
-                            var progress = Double(totalNumberReadPages) / Double(totalNumberOfPages)
-                            ProgressView(value: progress)
-                                        .progressViewStyle(LinearProgressViewStyle())
-                                        .frame(height: 8)
-                                        .padding(.horizontal)
-                                    
-                                    Text("Page \(totalNumberReadPages) of \(totalNumberOfPages)")
-                                        .font(.caption)
-                                        .foregroundColor(.gray)
-                                }
-                                .padding()
-                        
-                        
-                    }
-                    .swipeActions(edge: .trailing, allowsFullSwipe: false) {
-                        Button(role: .destructive) {
-                        } label: {
-                            Label("Delete", systemImage: "trash")
-                        }
-                        Button() {
-                            selectedBook = book
-                            isShowingEdit = true
-                        } label: {
-                            Label("Edit", systemImage: "square.and.arrow.up")
-                        }
-                    }
-                    .swipeActions(edge: .leading, allowsFullSwipe: false) {
-                        Button() {
-                            print("complete anime")
-                        } label: {
-                            Label("Complete", systemImage: "checkmark.circle.fill")
-                        }
-                        .tint(.green)
+        List(books) { book in
+            HStack {
+                AsyncImage(url: book.coverURL()) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                            .frame(width: 50, height: 50)
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                            .clipped()
+                    case .failure:
+                        Image(systemName: "photo")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                            .clipped()
+                    @unknown default:
+                        EmptyView()
                     }
                 }
-                .navigationDestination(isPresented: $isShowingEdit) {
-                    if let selectedBook = selectedBook {
-                        Text("Edit")
-                    }
+                
+                Spacer()
+                
+                Text(book.title)
+                
+                Spacer()
+                
+                VStack(spacing: 10) {
+                    var totalNumberOfPages = Int.random(in: 0...1000)
+                    var totalNumberReadPages = Int.random(in: 0...totalNumberOfPages)
+                    var progress = Double(totalNumberReadPages) / Double(totalNumberOfPages)
+                    ProgressView(value: progress)
+                        .progressViewStyle(LinearProgressViewStyle())
+                        .frame(height: 8)
+                        .padding(.horizontal)
+                    
+                    Text("Page \(totalNumberReadPages) of \(totalNumberOfPages)")
+                        .font(.caption)
+                        .foregroundColor(.gray)
                 }
-                .navigationTitle("Books")
+                .padding()
+                
+                
+            }
+            .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                Button(role: .destructive) {
+                } label: {
+                    Label("Delete", systemImage: "trash")
+                }
+                Button() {
+                    selectedBook = book
+                    isShowingEdit = true
+                } label: {
+                    Label("Edit", systemImage: "square.and.arrow.up")
+                }
+            }
+            .swipeActions(edge: .leading, allowsFullSwipe: false) {
+                Button() {
+                    print("complete anime")
+                } label: {
+                    Label("Complete", systemImage: "checkmark.circle.fill")
+                }
+                .tint(.green)
             }
         }
+    }
 }
 
 
--- Poketo/ContentView.swift
+++ Poketo/ContentView.swift
@@ -10,20 +10,77 @@ import SwiftUI
 struct ContentView: View {
     @StateObject private var booksModel = OpenLibraryBookViewModel()
     
+    @State private var selection = 0
+    
+    @State private var booksSearchText = ""
+    @State private var moviesSearchText = ""
+    @State private var seriesSearchText = ""
+
+    
     var body: some View {
-      NavigationStack {
-          if booksModel.isLoading {
-              ProgressView("Loading...")
-          } else if let error = booksModel.errorMessage {
-              Text(error)
-                  .foregroundColor(.red)
-          } else {
-              BookTracker(books: $booksModel.books)
-          }
+        TabView {
+            NavigationStack {
+                if booksModel.isLoading {
+                    ProgressView("Loading...")
+                } else if let error = booksModel.errorMessage {
+                    Text(error)
+                        .foregroundColor(.red)
+                } else {
+                    BookTracker(books: $booksModel.books)
+                    .navigationTitle("Books")
+                    .searchable(text: $booksSearchText, prompt: "Search Books")
+                    .onChange(of: booksSearchText) { oldValue, newValue in
+                        print("Books search text: \(newValue)")                    }
+                }
+            }
+            .task {
+                //await booksModel.seachBooksByTitle(title: "Uzumaki")
+            }
+            .tag(0)
+            .tabItem {
+                Label("Books", systemImage: "book")
+            }
+            
+            NavigationStack {
+                List {
+                    HStack {
+                        Text("Movies List")
+                    }
+                }
+                .navigationTitle("Movies")
+                .searchable(text: $moviesSearchText, prompt: "Search Movies")
+                .onChange(of: moviesSearchText) { oldValue, newValue in
+                    print("Movies search text: \(newValue)")
+                    // For now, no filtering
+                }
+                
+            }
+            .tag(1)
+            .tabItem {
+                Label("Movies", systemImage: "film")
+            }
+            
+            NavigationStack {
+                List {
+                    VStack {
+                        Text("Series List")
+                    }
+                }
+                .navigationTitle("Series")
+                .searchable(text: $seriesSearchText, prompt: "Search Series")
+                .onChange(of: seriesSearchText) { oldValue, newValue in
+                    print("Series search text: \(newValue)")
+                    // For now, no filtering
+                }
+            }
+            .tag(2)
+            .tabItem {
+                Label("Series", systemImage: "film")
+            }
         }
-      .task {
-          await booksModel.seachBooksByTitle(title: "Uzumaki")
-      }
+#if os(iOS)
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+#endif
     }
     
 }
